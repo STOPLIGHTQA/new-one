94e2389244d16373419e3470ecfb31bbb1c81fe1
Pankaj Yadav <pankaj.yadav@smartbear.com>
2025-12-29 11:27:12 +0530

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-20639.stoplight-dev.com/api/v1/projects/cHJqOjYwMg/spectral.js?branch_slug=main&token=53422692-8acc-43e0-80f2-70da55b78b00";
+export default { extends: ruleset };
